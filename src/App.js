93330ff5cfc41ec45b60e341a8277dc45a672fcf
Toanhp123 <toanpham12102003@gmail.com>
2024-09-18 22:22:39 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Board } from './components';
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <h1>Cờ Tướng </h1>
+            <Board />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import { default as Board } from './Board';
+import { default as Chess } from './Chess';
+
+export { Board, Chess };
