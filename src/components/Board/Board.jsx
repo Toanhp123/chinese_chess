2f9c36d38c9c210341aec681f844589dc80bf280
Toanhp123 <toanpham12102003@gmail.com>
2024-09-19 21:02:45 +0700

    test

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,14 +1,259 @@
 import './Board.css';
-
 import Chess from '../Chess/Chess';
-import { ba, board } from './import';
+
+import {
+    ba,
+    bb,
+    bc,
+    bk,
+    bkm,
+    bn,
+    bp,
+    br,
+    oo,
+    oos,
+    ra,
+    rb,
+    rc,
+    rk,
+    rkm,
+    rn,
+    rp,
+    rr,
+    thinking,
+} from './import';
+
+const MAX_COLS = 10;
+const MAX_ROWS = 9;
+
+const chessOnBoard = [
+    br,
+    bn,
+    bb,
+    ba,
+    bk,
+    ba,
+    bb,
+    bn,
+    br,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    bc,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    bc,
+    oo,
+    bp,
+    oo,
+    bp,
+    oo,
+    bp,
+    oo,
+    bp,
+    oo,
+    bp,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    rp,
+    oo,
+    rp,
+    oo,
+    rp,
+    oo,
+    rp,
+    oo,
+    rp,
+    oo,
+    rc,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    rc,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    oo,
+    rr,
+    rn,
+    rb,
+    ra,
+    rk,
+    ra,
+    rb,
+    rn,
+    rr,
+];
 
 const Board = () => {
+    function setupBoardSquares() {}
+
     return (
         <div className="chinese-chess section__padding">
             <div className="chinese-chess__board">
-                <img src={board} alt="board" />
-                <Chess typeChess={ba} />
+                {/* 10th rank */}
+                <div className="chinese-chess__board--square__row">
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                </div>
+
+                {/* 9th rank */}
+                <div className="chinese-chess__board--square__row">
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                </div>
+
+                {/* 8th rank */}
+                <div className="chinese-chess__board--square__row">
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                </div>
+
+                {/* 7th rank */}
+                <div className="chinese-chess__board--square__row">
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                </div>
+
+                {/* 6th rank */}
+                <div className="chinese-chess__board--square__row">
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                </div>
+
+                {/* 5th rank */}
+                <div className="chinese-chess__board--square__row">
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                </div>
+
+                {/* 4th rank */}
+                <div className="chinese-chess__board--square__row">
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                </div>
+
+                {/* 3rd rank */}
+                <div className="chinese-chess__board--square__row">
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                </div>
+
+                {/* 2nd rank */}
+                <div className="chinese-chess__board--square__row">
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                </div>
+
+                {/* 1st rank */}
+                <div className="chinese-chess__board--square__row">
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                    <div className="chinese-chess__board--square"></div>
+                </div>
             </div>
         </div>
     );
