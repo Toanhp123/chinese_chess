44d97011dde697313a3ee975ecc0115f9e21fb49
Toanhp123 <toanpham12102003@gmail.com>
2024-09-20 12:20:13 +0700

    test

--- src/utils/checkMove/IsValidMove.js
+++ src/utils/checkMove/IsValidMove.js
@@ -4,9 +4,9 @@ const isValidMove = (piece, fromRow, fromCol, toRow, toCol) => {
     // Không cho ở lại vị trí cũ
     if (fromRow === toRow && fromCol === toCol) return false;
 
-    let NamePiece = piece.name;
+    let namePiece = piece.name;
 
-    switch (NamePiece) {
+    switch (namePiece) {
         // Quy tắc di chuyển của quân Xe (Rook)
         case 'xe':
             return Rook(fromRow, fromCol, toRow, toCol);
--- src/utils/setupMove/Soldier.js
+++ src/utils/setupMove/Soldier.js
@@ -1,5 +1,10 @@
 import chessOnBoard from '../setupBoard/chessOnBoard';
 
-const isSoldierMoveValid = (fromRow, fromCol, toRow, toCol) => {};
+const isSoldierMoveValid = (fromRow, fromCol, toRow, toCol) => {
+    // Quân tốt di chuyển thẳng
+    if (fromCol !== toCol) return false;
+
+    return true;
+};
 
 export default isSoldierMoveValid;
