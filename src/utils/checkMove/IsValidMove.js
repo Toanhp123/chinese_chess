9313da206582c7994102e2bab99ef4c9166d44c3
Toanhp123 <toanpham12102003@gmail.com>
2024-09-20 23:23:47 +0700

    test

--- src/utils/checkMove/IsValidMove.js
+++ src/utils/checkMove/IsValidMove.js
@@ -1,4 +1,4 @@
-import { Rook, Soldier, Advisor, Horse } from '../setupMove';
+import { Rook, Soldier, Advisor, Horse, Elephant } from '../setupMove';
 
 const isValidMove = (piece, fromRow, fromCol, toRow, toCol, board) => {
     // Không cho ở lại vị trí cũ
@@ -17,6 +17,8 @@ const isValidMove = (piece, fromRow, fromCol, toRow, toCol, board) => {
             return Advisor(pieceColor, fromRow, fromCol, toRow, toCol);
         case 'mã':
             return Horse(fromRow, fromCol, toRow, toCol, board);
+        case 'tịnh':
+            return Elephant(fromRow, fromCol, toRow, toCol, board);
         default:
             return false;
     }
---
+++ src/utils/setupMove/Elephant.js
@@ -0,0 +1,3 @@
+const isElephantMoveValid = () => {};
+
+export default isElephantMoveValid;
