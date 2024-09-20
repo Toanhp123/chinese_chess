ede38723222a1037fbefd531ea79c2a21ebcb2a0
Toanhp123 <toanpham12102003@gmail.com>
2024-09-20 12:08:49 +0700

    test

--- src/utils/setupMoveChess/Rook.js
+++ src/utils/setupMoveChess/Rook.js
@@ -8,12 +8,14 @@ const isRookMoveValid = (fromRow, fromCol, toRow, toCol) => {
     if (fromRow === toRow) {
         // Di chuyển ngang.
         const [min, max] = [Math.min(fromCol, toCol), Math.max(fromCol, toCol)];
+
         for (let col = min + 1; col < max; col++) {
             if (chessOnBoard[fromRow][col] !== '') return false;
         }
     } else {
         // Di chuyển dọc.
         const [min, max] = [Math.min(fromRow, toRow), Math.max(fromRow, toRow)];
+
         for (let row = min + 1; row < max; row++) {
             if (chessOnBoard[row][fromCol] !== '') return false;
         }
