04fc7e429fd365c6d5d2388a3085836510713250
Toanhp123 <toanpham12102003@gmail.com>
2024-09-20 10:22:44 +0700

    test

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -42,19 +42,18 @@ const Board = () => {
     }
 
     function handleDrop(e, row, col) {
-        if (
-            draggedPiece &&
-            (draggedPiece.row !== row || draggedPiece.col !== col)
-        ) {
+        if (draggedPiece) {
             const newBoard = board.map((row) => [...row]);
 
             if (
                 newBoard[row][col].color !==
                 board[draggedPiece.row][draggedPiece.col].color
             ) {
+                // Setup vị trí ở bảng mới
                 newBoard[row][col] = board[draggedPiece.row][draggedPiece.col];
                 newBoard[draggedPiece.row][draggedPiece.col] = '';
 
+                // Render lại bảng
                 setBoard(newBoard);
                 setDraggedPiece(null);
                 setIsRedTurn((prev) => !prev);
@@ -66,9 +65,11 @@ const Board = () => {
         const pieceColor = e.target.getAttribute('color');
         const img = new Image();
 
+        // Giúp kéo theo cả ảnh
         img.src = board[row][col].image;
         e.dataTransfer.setDragImage(img, 24, 24);
 
+        // Kiểm tra đang ở lượt bên nào
         if (
             (isRedTurn && pieceColor === 'red') ||
             (!isRedTurn && pieceColor === 'black')
@@ -76,6 +77,45 @@ const Board = () => {
             setDraggedPiece({ row, col });
         }
     }
+
+    const isValidMove = (piece, fromRow, fromCol, toRow, toCol) => {
+        // Không cho ở lại vị trí cũ
+        if (fromRow === toRow && fromCol === toCol) return false;
+        switch (piece) {
+            case 'R': // Quy tắc di chuyển của quân Xe (Rook)
+                return isRookMoveValid(fromRow, fromCol, toRow, toCol);
+            // Thêm quy tắc di chuyển cho các quân cờ khác.
+            default:
+                return false;
+        }
+    };
+
+    const isRookMoveValid = (fromRow, fromCol, toRow, toCol) => {
+        // Quân Xe di chuyển theo hàng ngang hoặc dọc.
+        if (fromRow !== toRow && fromCol !== toCol) return false;
+
+        // Kiểm tra đường đi không bị chặn.
+        if (fromRow === toRow) {
+            // Di chuyển ngang.
+            const [min, max] = [
+                Math.min(fromCol, toCol),
+                Math.max(fromCol, toCol),
+            ];
+            for (let col = min + 1; col < max; col++) {
+                if (board[fromRow][col] !== '') return false;
+            }
+        } else {
+            // Di chuyển dọc.
+            const [min, max] = [
+                Math.min(fromRow, toRow),
+                Math.max(fromRow, toRow),
+            ];
+            for (let row = min + 1; row < max; row++) {
+                if (board[row][fromCol] !== '') return false;
+            }
+        }
+        return true;
+    };
 
     return (
         <div className="chinese-chess">
