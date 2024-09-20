10c8f9288922a60273273bc7e20e99f0c9388c0a
Toanhp123 <toanpham12102003@gmail.com>
2024-09-20 09:57:06 +0700

    test

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import { default as Board } from './Board/Board.jsx';
 import { default as Chess } from './Chess/Chess.jsx';
+import { default as Square } from './Square/Square.jsx';
 
-export { Board, Chess };
+export { Board, Chess, Square };
