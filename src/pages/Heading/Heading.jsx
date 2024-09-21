3c6b3aa291baddedefda2b23643aa5d6ee7e0fa6
Toanhp123 <toanpham12102003@gmail.com>
2024-09-21 16:25:56 +0700

    test

--- src/pages/Heading/Heading.jsx
+++ src/pages/Heading/Heading.jsx
@@ -1,17 +1,24 @@
 import './Heading.css';
 
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { StoreContext } from '../../store';
 
 const Heading = () => {
     const { isRedTurn } = useContext(StoreContext);
+    const [whoTurn, setWhoTurn] = useState('');
 
-    console.log(isRedTurn);
+    useEffect(() => {
+        if (isRedTurn) {
+            setWhoTurn('bạn');
+        } else {
+            setWhoTurn('máy');
+        }
+    }, [isRedTurn]);
 
     return (
         <div className="chinese-chess__heading">
             <h1>Cờ Tướng</h1>
-            <h2>Lượt của {isRedTurn ? 'bạn' : 'máy'}</h2>
+            <h2>Lượt của {whoTurn}</h2>
         </div>
     );
 };
