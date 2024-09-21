77e39121ccfb4c6898f010418921a38480a9b91f
Toanhp123 <toanpham12102003@gmail.com>
2024-09-21 09:56:56 +0700

    test

--- src/utils/setupMove/Soldier.js
+++ src/utils/setupMove/Soldier.js
@@ -1,4 +1,10 @@
 const isSoldierMoveValid = (pieceColor, fromRow, fromCol, toRow, toCol) => {
+    // TODO: có lỗi đi lùi khi qua sông cần sửa
+
+    // Tính toán khoảng cách hàng và cột.
+    const rowDiff = toRow - fromRow;
+    const colDiff = toCol - fromCol;
+
     // Quân đỏ hướng -1 đen hướng 1 chiều dọc
     const direction = pieceColor === 'red' ? -1 : 1;
 
@@ -6,15 +12,18 @@ const isSoldierMoveValid = (pieceColor, fromRow, fromCol, toRow, toCol) => {
     const isAcrossRiver = pieceColor === 'red' ? fromRow < 5 : fromRow > 4;
 
     // Quân đen đi xuống, quân đỏ đi lên.
-    if (fromCol === toCol && toRow - fromRow === direction) {
+    if (colDiff === 0 && rowDiff === direction) {
         return true;
     }
 
     // Khi qua sông
     if (isAcrossRiver) {
-        // Đi thẳng hoặc ngang 1 ô.
-        const rowMove = Math.abs(toRow - fromRow) === 1 && toCol === fromCol;
-        const colMove = toRow === fromRow && Math.abs(toCol - fromCol) === 1;
+        // Đi thẳng 1 ô.
+        const rowMove = rowDiff === direction && toCol === fromCol;
+
+        //  Đi ngang 1 ô.
+        const colMove = rowDiff === 0 && Math.abs(colDiff) === 1;
+        
         return rowMove || colMove;
     }
 
