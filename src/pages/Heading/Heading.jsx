fd7f77560c4c94a3d803a7ec080f49e20fe30626
Toanhp123 <toanpham12102003@gmail.com>
2024-09-21 16:14:21 +0700

    test

--- src/pages/Heading/Heading.jsx
+++ src/pages/Heading/Heading.jsx
@@ -1,9 +1,17 @@
 import './Heading.css';
 
+import { useContext } from 'react';
+import { StoreContext } from '../../store';
+
 const Heading = () => {
+    const { isRedTurn } = useContext(StoreContext);
+
+    console.log(isRedTurn);
+
     return (
         <div className="chinese-chess__heading">
-            <h1>Cờ Tướng </h1>
+            <h1>Cờ Tướng</h1>
+            <h2>Lượt của {isRedTurn ? 'bạn' : 'máy'}</h2>
         </div>
     );
 };
